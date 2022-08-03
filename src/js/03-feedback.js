0a8b9bc8d259cbdaed7d953b5350234960dd0898
EnVy <envy7321@gmail.com>
2022-08-03 11:29:44 +0300

    fixed

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -5,17 +5,21 @@ const form = document.querySelector('.feedback-form');
 form.addEventListener('input', Throttle(onForm, 500));
 form.addEventListener('submit', onSubmitButton);
 
-const feedbackForm = {};
-
-function onForm(event) {
-    const { name, value } = event.target;
-    feedbackForm[name] = value;
+dataFromLocalStorage();
+let feedbackForm = {};
+function onForm() {
+   
+    feedbackForm={
+        email: form.elements.email.value,
+        message: form.elements.message.value,
+    }
+   
     localStorage.setItem('feedback-form-state', JSON.stringify(feedbackForm));
 };
 
 function onSubmitButton(event) {
     event.preventDefault();
-    console.log(JSON.parse(localStorage.getItem('feedback-form-state')));
+    console.log(feedbackForm);
     form.reset();
     localStorage.removeItem('feedback-form-state');
     
@@ -23,11 +27,9 @@ function onSubmitButton(event) {
 
 function dataFromLocalStorage() {
     const dataLocalStorage = JSON.parse(localStorage.getItem('feedback-form-state'));
-    
-     if (dataLocalStorage) {
+      if (dataLocalStorage) {
         form.elements.email.value = dataLocalStorage.email;
         form.elements.message.value = dataLocalStorage.message;
     };
 
 };
-dataFromLocalStorage();
